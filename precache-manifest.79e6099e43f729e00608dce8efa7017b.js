20f5e334283101a442ca0f0375fe6f82fed5da51
PRAVEENKUMARPATNALA <patnalapraveenkumar@gmail.com>
2020-10-06 22:07:47 +0530

    Updates

---
+++ precache-manifest.79e6099e43f729e00608dce8efa7017b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fb4176511a425fd3ea97ce58cc84d1",
+    "url": "/reactdom/index.html"
+  },
+  {
+    "revision": "4353f623d0bb742d456d",
+    "url": "/reactdom/static/css/main.82a73193.chunk.css"
+  },
+  {
+    "revision": "111924ce1a277bd48e1b",
+    "url": "/reactdom/static/js/2.1ee5bc40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactdom/static/js/2.1ee5bc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4353f623d0bb742d456d",
+    "url": "/reactdom/static/js/main.dcd98c00.chunk.js"
+  },
+  {
+    "revision": "eba28a98e16f83e09379",
+    "url": "/reactdom/static/js/runtime-main.db9b8a54.js"
+  },
+  {
+    "revision": "28dbaa3dfacc440d5d02097ca1044630",
+    "url": "/reactdom/static/media/praveen.28dbaa3d.jpg"
+  }
+]);
